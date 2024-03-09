29f07694b03d53ffcdf3bee0becad24e2cf991f1
A.M <aimirook.on@gmail.com>
2024-03-09 17:25:43 +0900

    振り返り：基本的な型定義

---
+++ typescript-react-tutorial/src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import { Interface } from 'readline';
+
+const name = "hello";
+
+let nameChange = "hello";
+nameChange = "hello2";
+
+let username: string = "Hello";
+let dummyNum: number = 2;
+let bool: boolean = true;
+
+let array1 = [true, false, true];
+let array2 = [0, 1, "hello"];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+let nameOBJ: NAME = {first:"Yamada", last: null}
+
+// 関数の後に「： number」と定義する事で返り値にも明示的に型を記述可能
+const func1 = (x: number, y: number): number => {
+  return x + y;
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
