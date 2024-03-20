00e2c8a35ebea07ae072727872f2bb77881d403a
A.M <aimirook.on@gmail.com>
2024-03-20 17:23:26 +0900

    reactでTypeScriptを使用したtodoApp 土台

---
+++ typescript-react-todoapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './destyle.scss';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="default">
+        <p>sample text</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
